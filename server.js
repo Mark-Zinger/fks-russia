5c52653c683fa968846e884790906f5f664f41cc
Mark Zinger <markzingergithub@gmail.com>
2021-03-04 21:37:46 +0900

    add main route

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-const port = 3000
 const db = require('./models');
 let session = require('express-session');
+
+const app = express();
+const port = 3000
+
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "src/pages"));
@@ -17,19 +19,12 @@ app.use(session({
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-    // db.User.findOne()
-    //   .then((data)=> {
-    //       console.log(data)
-    //       req.session.save(function(err) {
-    //         // session saved
-    //       })
-    //       res.render('homepage', {...data.dataValues} )
-    //   })
-    //   .catch(console.error)
-    res.render('homepage')
-})
+const mainRoute = require("./routes/main");
+
+app.use(mainRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+exports.app = app;
