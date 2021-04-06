d2b825c508736d1cf265fa3d27c4ad3af7d6b7c7
Mark Zinger <markzingergithub@gmail.com>
2021-04-06 11:08:52 +0900

    fix

--- client/src/components/TournamentSlider/introTour.js
+++ client/src/components/TournamentSlider/introTour.js
@@ -13,25 +13,6 @@ const IntroTour = (params) => {
   const {game, fond,dateBegin} = useContext(TournamentConetext);
   console.log(game);
 
-  const CountDownRender = ({ days,hours, minutes }) => {
-    
-  }
-
-  const renderer = ({ days,hours, minutes }) => (
-    <div className="tournament-intro__time">
-      <div className="tournament-intro__time">
-        <div className="tournament-intro__time">{zeroPad(days)}</div><br/> Дней
-      </div>
-      <div className="tournament-intro__time">
-        {zeroPad(hours)}<br/> Часов
-      </div>
-      <div className="tournament-intro__time">
-        {zeroPad(minutes)}<br/> Минут
-      </div>
-  
-  
-    </div>
-  );
 
  return (
     <div 
@@ -44,25 +25,52 @@ const IntroTour = (params) => {
         <div className="date date__start-tour">Начало турнира<br/>02.04.2021</div>
         <div className="date date__end-tour">Окончание турнира <br/>02.04.2021</div>
       </div>
-      <div className="tournament-intro__time">
-        {dateBegin &&<CountDown date={dateBegin} renderer={renderer}/> }
+     
+     <div className="tournament-intro__data">
+      <div className="tournament-intro__time__and__team">
+          {dateBegin &&<CountDown date={dateBegin} renderer={CountDownRender}/> }
+            <div className="tournament-intro__comands">
+              Участники:
+              <div>
+                16 / 24
+              </div>
+            </div>
       </div>
+      
       <div className="tournament-intro__fond">
         Призовой фонд: 
         <motion.div
           className="tournament-intro__fond-money"
-        
         >
          {fond && <CounterTwo from={0} to={parseInt(fond)} />}  
         </motion.div>
       </div>
-      <Button>Принять Учатсие</Button>
-
+      <Button className="tournament-intro__button">Принять Учатсие</Button>
+     </div>
     </div>
 
  ) 
 }
 
+const CountDownRender = ({ days,hours, minutes }) => (
+  <div className="tournament-intro__time">
+    До начала турнира:
+    <div className="tournament-intro__time-container">
+      <div className="tournament-intro__time-date">
+        <div className="tournament-intro__time-number">{zeroPad(days)}</div>
+        <div className="tournament-intro__time-title">Дней</div> 
+      </div>
+      <div className="tournament-intro__time-date">
+        <div className="tournament-intro__time-number">{zeroPad(hours)}</div>
+        <div className="tournament-intro__time-title">Часов</div>
+      </div>
+      <div className="tournament-intro__time-date">
+        <div className="tournament-intro__time-number">{zeroPad(minutes)}</div>
+        <div className="tournament-intro__time-title">Минут</div>
+      </div>
+    </div>
+  </div>
+);
 
 
 export default IntroTour;
