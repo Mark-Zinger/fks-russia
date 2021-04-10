6b8960de40e831b4ae23acb755dac18fa8c56c1c
Mark Zinger <markzingergithub@gmail.com>
2021-04-10 12:44:42 +0900

    fix

--- src/pages/api/tournaments/index.js
+++ src/pages/api/tournaments/index.js
@@ -1,13 +1,39 @@
+import nextConnect from 'next-connect';
+const { Tournaments, Game, MainPageSlider} = require('../models');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
-import {Tournaments} from '../../../../db/models';
+const handler = nextConnect()
+  .get(async (req, res) => {
 
-// import { Tournaments, Game, MainPageSlider} from '../../../../db/models';
+    const limit = req.query.limit? parseInt(req.query.limit) : 10;
+    const search = req.query.search? req.query.search : false;
+    const game = req.query.game? parseInt(req.query.game) : false;
+    const page = req.query.page? req.query.page: 1;
+    console.log(limit,search, game, req.query.game, page);
 
-const handler = async (req, res) => {
-  // console.log(__dirname);
-  const tournaments = await Tournaments.findAll();
-  res.json(tournaments)
-  // res.json({test: 'ts'})
-}
+    const where = {};
+  
+    if(search) where.name = {[Op.like]: `%${search}%`};
+    if(game) where.id_game = game;
+  
+    const dbQuery = {
+      limit,
+      include: [{ model: Game, as: 'game'}],
+      where
+    }
+
+    try {
+      const tournaments = await Tournaments.findAll(dbQuery)
+  
+      res.json(tournaments);
+    } catch (e) {
+      res.status(500).json(e);
+    }  
+  })
+    
+    res.json({query,method, body,user})
+  })
+
 
 export default handler;
